139f1b3f6b18c7181af8f6a196f666b7e58f8d18
Pearl Calice <celinecossez@SugarHillGang.local>
2019-04-10 15:07:24 +0200

    adding image to home

--- my_movies_lists/src/Controller/HomeController.php
+++ my_movies_lists/src/Controller/HomeController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class HomeController
 {
-
     /**
      * @var Environment
      */
@@ -16,25 +16,23 @@ class HomeController
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;
-    }
+    } 
+
 
     public function index():Response
     {
         $token  = new \Tmdb\ApiToken('2d0f20ce4b63a3992d225fe3751cb1d1');
         $client = new \Tmdb\Client($token);
-        /* $repository = new \Tmdb\Repository\MovieRepository($client);
-        $movie      = $repository->load(552); */
-
-        $configRepository = new \Tmdb\Repository\ConfigurationRepository($client);
-        $config = $configRepository->load();
-
-        $imageHelper = new \Tmdb\Helper\ImageHelper($config);
-        $images = $client->getMoviesApi()->getImages(550);
-        //$imageHelper->getHtml($images, 'w154', 154, 80);
-
         
-        return new Response($this->twig->render('Pages/Home.html.twig'));
+        $movies = $client->getMoviesApi()->getPopular();
+        /* $repository = new \Tmdb\Repository\MovieRepository($client);
+        $movies = $repository->getTopRated(array('page' => 1)); */
+        
+        dump($movies);
         //return new Response($this->twig->render('Pages/Home.html.twig'));
-    }
+        return new Response($this->twig->render('Pages/Home.html.twig', array('movies' => $movies)));
+        
+    
 
+    }
 }
